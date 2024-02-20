1a00fc1a0c366e33aa49480260bce94a01a32df4
Alan Smyth <alan_smyth@hotmail.com>
2024-02-20 22:30:04 +0000

    Add the function resetstate and selectanswer, add correct,incorrect to css

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,7 +90,7 @@ const questions = [
     },
 
     {
-        question: "10 what is the sign for this a image I?",
+        question: "10what is the sign for this a image I?",
         answers: [
           {text: "F", correct: false},
           {text: "G", correct: false},
@@ -101,7 +101,7 @@ const questions = [
     ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -113,6 +113,7 @@ function startQuiz (){
 }
 
 function showQuestion(){
+  resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -121,9 +122,37 @@ function showQuestion(){
     const button = document.createElement("button");
     button.innerHTML = answer.text;
     button.classList.add("btn")
-    answerButton.appendChild(button);
+    answerButtons.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
   });
 
 }
 
-startQuiz();
+function resetState(){
+  nextButton.style.display = "none";
+  while(answerButtons.firstChild){
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e){
+  const selectBtn = e.target;
+  const isCorrect = selectBtn.dataset.correct === "true";
+  if(isCorrect){
+      selectBtn.classList.add("correct");
+}else{
+      selectBtn.classList.add("incorrect");
+}
+Array.from(answerButtons.children).forEach(button => {
+  if(button.dataset.correct === "true"){
+      button.classList.add("correct");
+  }
+  button.disabled = true;
+});
+nextButton.style.display = "block";
+}
+
+startQuiz()
